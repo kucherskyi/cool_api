2a693da03d84126e23dfb1574166dcc3c1579a96
kucherskyi <a.kucherskyy@gmail.com>
2016-08-12 18:09:55 +0300

    temporary commit

--- app/controllers/comments.py
+++ app/controllers/comments.py
@@ -11,36 +11,38 @@ class Comments(MethodView):
     decorators = [auth_token_required]
 
     def get(self, task_id):
-        if self.is_assign(task_id):
-            connection = psycopg2.connect(self.connect())
-            get_comments = connection.cursor()
-            query = 'SELECT user_id, text FROM comments WHERE task_id=%s'
-            get_variables = (task_id, )
-            get_comments.execute(query, get_variables)
-            records = get_comments.fetchall()
-            column_names = [x[0] for x in get_comments.description]
-            data = []
-            for item in records:
-                data.append(dict(zip(column_names, item)))
-            return jsonify(data)
-        abort(400)
+        if not Comments.is_assign(current_app.user.id, task_id):
+            abort(400)
+        connection = psycopg2.connect(self.connect())
+        get_comments = connection.cursor()
+        query = 'SELECT user_id, text FROM comments WHERE task_id=%s'
+        get_variables = (task_id, )
+        get_comments.execute(query, get_variables)
+        records = get_comments.fetchall()
+        column_names = [x[0] for x in get_comments.description]
+        data = []
+        for item in records:
+            data.append(dict(zip(column_names, item)))
+        return jsonify(data)
+        get_comments.close()
 
     def post(self, task_id):
-        if self.is_assign(task_id):
-            connection = psycopg2.connect(self.connect())
-            comment = json.loads(request.data)['comment']
-            post_comments = connection.cursor()
-            query = '''INSERT INTO comments
-                                   (text, user_id, task_id, created_at)
-                            VALUES (%s,%s,%s,%s)'''
-            post_variables = (comment,
-                              current_app.user.id,
-                              task_id,
-                              datetime.utcnow(),)
-            post_comments.execute(query, post_variables)
-            connection.commit()
-            post_comments.close()
-            return jsonify({'comment': comment}), 201
+        if not Comments.is_assign(current_app.user.id, task_id):
+            abort(400)
+        connection = psycopg2.connect(self.connect())
+        comment = json.loads(request.data)['text']
+        post_comments = connection.cursor()
+        query = '''INSERT INTO comments
+                               (text, user_id, task_id, created_at)
+                        VALUES (%s,%s,%s,%s)'''
+        post_variables = (comment,
+                          current_app.user.id,
+                          task_id,
+                          datetime.utcnow(),)
+        post_comments.execute(query, post_variables)
+        connection.commit()
+        post_comments.close()
+        return jsonify({'text': comment}), 201
 
     @staticmethod
     def connect():
@@ -54,14 +56,15 @@ class Comments(MethodView):
         return conn_string
 
     @staticmethod
-    def is_assign(task_id):
+    def is_assign(user_id, task_id):
         is_assigned = Comments.connect()
         connection = psycopg2.connect(is_assigned)
         check = connection.cursor()
         query = '''SELECT * FROM user_task_relation
                            WHERE user_id = %s AND task_id = %s'''
-        condition = (current_app.user.id, task_id,)
+        condition = (user_id, task_id,)
         check.execute(query, condition)
         if check.fetchall() == []:
             return False
         return True
+        check.close()
---
+++ tests/test_comments.py
@@ -0,0 +1,65 @@
+import unittest
+import hashlib
+
+from tests.base_test import BaseTestCase
+from app.models.user import db, User
+from app.models.task import Task
+from app.models.comment import Comment
+
+
+class TestComments(BaseTestCase):
+
+    ENDPOINT = 'api/tasks'
+
+    def test_get_comments_no_auth(self):
+        self.assert401(self.client.get(self.ENDPOINT))
+
+    def test_get_comments_for_not_existing_task(self):
+        token = self.login().json['token']
+        res = self.client.get(self.ENDPOINT + '/1/comments',
+                              headers={'token': token})
+        self.assert400(res)
+
+    def test_get_comments_for_not_assigned_task(self):
+        task1 = Task(title='title1', status='in_progress')
+        task1.assign_user(1)
+        comment1 = Comment(text='yey', user_id=1, task_id=1)
+        db.session.add(task1)
+        db.session.add(comment1)
+        db.session.commit()
+        user2 = User(name='user2', email='email2@em.co')
+        user2.password = hashlib.md5('1').hexdigest()
+        db.session.add(user2)
+        db.session.commit()
+        token = self.login('user2', '1').json['token']
+        res = self.client.get(self.ENDPOINT + '/1/comments',
+                              headers={'token': token})
+        self.assert400(res)
+
+    def test_get_comments_for_assigned_task(self):
+        task1 = Task(title='title1', status='in_progress')
+        task1.assign_user(1)
+        comment1 = Comment(text='yey', user_id=1, task_id=1)
+        db.session.add(task1)
+        db.session.add(comment1)
+        db.session.commit()
+        token = self.login().json['token']
+        res = self.client.get(self.ENDPOINT + '/1/comments',
+                              headers={'token': token})
+        self.assert200(res)
+        self.assertDictEqual(res.json[0], {'text': 'yey',
+                                           'user_id': 1})
+        self.assertEqual(len(res.json), 1)
+
+    def test_post_comments_for_assigned_task(self):
+        task1 = Task(title='title1', status='in_progress')
+        task1.assign_user(1)
+        db.session.add(task1)
+        db.session.commit()
+        token = self.login().json['token']
+        res = self.client.post(self.ENDPOINT + '/1/comments',
+                               headers={'token': token},
+                               data={'text': 'yey',
+                                     'user_id': 1,
+                                     'task_id': 1})
+        self.assert201(res)
