d383e9d05eee2cd4cbdad2639b14a76f0af96ac6
kucherskyi <a.kucherskyy@gmail.com>
2016-09-19 17:53:47 +0300

    heroku

--- config/prod.py
+++ config/prod.py
@@ -3,5 +3,5 @@ DEBUG = True
 TESTING = False
 CSRF_ENABLED = True
 SECRET_KEY = 'sk'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgres://hmnxxcyzoqgkwr:wMOmPdJXFcHbXxd_aInv7NMXPJ@ec2-54-243-48-178.compute-1.amazonaws.com:5432/d1r4cu5l4bva9u'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
