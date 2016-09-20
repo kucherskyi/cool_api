4566ea9466694a5722c9a45afd8e0e4bc0d4cedd
kucherskyi <a.kucherskyy@gmail.com>
2016-09-20 11:23:20 +0300

    heroku

--- config/prod.py
+++ config/prod.py
@@ -2,5 +2,5 @@ DEBUG = True
 TESTING = False
 CSRF_ENABLED = True
 SECRET_KEY = 'sk'
-SQLALCHEMY_DATABASE_URI = 'postgres://blcebagdkaovwe:x_Oc1rSW_bkQOtZmKHLhxA1CWB@ec2-50-17-249-147.compute-1.amazonaws.com:5432/d6q0gl7d48tc8q'
+SQLALCHEMY_DATABASE_URI = 'postgres://hmnxxcyzoqgkwr:wMOmPdJXFcHbXxd_aInv7NMXPJ@ec2-54-243-48-178.compute-1.amazonaws.com:5432/d1r4cu5l4bva9u'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
