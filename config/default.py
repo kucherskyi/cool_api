9aad649a33a4d552607a3d0760077ac1cc55cc37
kucherskyi <a.kucherskyy@gmail.com>
2016-09-20 10:54:49 +0300

    heroku

--- config/default.py
+++ config/default.py
@@ -1,3 +1,3 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://admin:pass@localhost:5432/test_migrations'
+SQLALCHEMY_DATABASE_URI = 'postgres://blcebagdkaovwe:x_Oc1rSW_bkQOtZmKHLhxA1CWB@ec2-50-17-249-147.compute-1.amazonaws.com:5432/d6q0gl7d48tc8q'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
